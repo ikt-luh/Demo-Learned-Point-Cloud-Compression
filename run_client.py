bf22eca5d09152054ff7b0275ea4944a316b2b34
Michael Rudolph <michael.rudolph@ikt.uni-hannover.de>
2024-12-16 11:29:27 +0100

    Removed old startup scripts

--- run_client.py
+++
@@ -1,6 +0,0 @@
-from client.client import StreamingClient 
-
-mpd_url = 'http://localhost:8000/manifest.mpd'  # Your MPD URL
-base_url = 'http://localhost:8000/'  # Your MPD URL
-client = StreamingClient(base_url, mpd_url, "downloaded_segments")
-client.stream()
