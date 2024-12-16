cf0f4ae5501140c2cee0ffb5b886dcaaa6be9a3b
Michael Rudolph <michael.rudolph@ikt.uni-hannover.de>
2024-12-16 16:16:08 +0100

    Refactoring media server

--- sender/media_server/media_server.py
+++ sender/media_server/media_server.py
@@ -13,7 +13,7 @@ shared_epoch = datetime(2024, 1, 1, 0, 0, 0).timestamp()
 
 
 class StreamingServer:
-    def __init__(self, ip_addr, output_directory="./media", zmq_address="tcp://*:5556", port=8080, segment_duration=1.0):
+    def __init__(self, ip_addr, port=8080, output_directory="./media", segment_duration=1.0):
         # Setup directories and MPD Manager
         self.ip_addr = ip_addr
         self.output_directory = output_directory
@@ -22,16 +22,13 @@ class StreamingServer:
         self.mpd_manager = MPDManager(self.output_directory)
         self.mpd_manager.setup_adaptation_set()
 
-        # Setup ZeroMQ
         self.context = zmq.Context()
         self.pull_socket = self.context.socket(zmq.PULL)
-        self.pull_socket.bind(zmq_address)
+        self.pull_socket.bind("tcp://*:5556")
 
         self.port = port
         self.segment_duration = segment_duration
-
-        # Buffer for segment data
-        self.segment_buffer = []  # {segment_number: [data1, data2, ...]}
+        self.segment_buffer = [] 
         self.lock = threading.Lock()
 
     def start_http_server(self):
@@ -51,9 +48,9 @@ class StreamingServer:
 
         while True:
             serialized_data = self.pull_socket.recv()
-            print("Received {}".format(time.time()))
-            sys.stdout.flush()
+            print("Received {}".format(time.time()), flush=True)
             data = self.deserialize_data(serialized_data)
+
             with self.lock:
                 self.segment_buffer.append(data)
 
@@ -68,9 +65,7 @@ class StreamingServer:
 
         while True:
             timestamp = datetime.now().timestamp()
-            current_segment = math.floor(timestamp)
-            next_segment = current_segment + 1
-            next_segment_time = shared_epoch + next_segment * self.segment_duration
+            current_segment = math.floor(timestamp/self.segment_duration)
 
             # Process and flush the current segment
             with self.lock:
@@ -78,25 +73,23 @@ class StreamingServer:
                     data = self.segment_buffer.pop(0)
                     threading.Thread(target=self.handle_data, args=(data, current_segment)).start()
                 else:
-                    print("empty buffer")
+                    print("Empty buffer, waiting for data", flush=True)
 
-            current_segment = next_segment
-        
             sleep_time = max(0, self.segment_duration - (datetime.now().timestamp() - timestamp))
             time.sleep(sleep_time)
-            print("Running")
+
 
     def cleanup_segments(self):
         num_reps = 3 # TODO
         while True:
             timestamp = datetime.now().timestamp()
-            number = math.floor((timestamp) / self.segment_duration) - 10
+            old_segment_number = math.floor((timestamp) / self.segment_duration) - 10
+
             for key in range(num_reps):
                 segment_folder = os.path.join(self.output_directory, f"ID{key}")
-                segment_path = os.path.join(segment_folder, f"segment-{number:015d}.bin")
+                segment_path = os.path.join(segment_folder, f"segment-{old_segment_number:015d}.bin")
                 if os.path.exists(segment_path):
                     os.remove(segment_path)
-                sys.stdout.flush()
 
             sleep_time = max(0, self.segment_duration - (datetime.now().timestamp() - timestamp))
             time.sleep(sleep_time)
@@ -107,21 +100,22 @@ class StreamingServer:
         Handle the data
         """
         _ = data.pop("timestamp", None)
-        segment_duration = data.pop("segment_duration", None)
-        frame_rate = data.pop("frame_rate", None)
+        _ = data.pop("segment_duration", None)
+        _ = data.pop("frame_rate", None)
 
-        number = math.floor((timestamp) / self.segment_duration)
+        segment_number = math.floor((timestamp) / self.segment_duration)
 
         for key, item in data.items():
             segment_folder = os.path.join(self.output_directory, f"ID{key}")
-            segment_path = os.path.join(segment_folder, f"segment-{number:015d}.bin")
-            tmp_segment_path = os.path.join(segment_folder, f"segment-{number:015d}_tmp.bin")
+            segment_path = os.path.join(segment_folder, f"segment-{segment_number:015d}.bin")
+            tmp_segment_path = os.path.join(segment_folder, f"segment-{segment_number:015d}_tmp.bin")
             os.makedirs(segment_folder, exist_ok=True)
 
             # Write the segment to disk
-            with open(tmp_segment_path, "wb") as f:
-                pickle.dump(item, f)
-            os.rename(tmp_segment_path, segment_path)
+            with self.lock:
+                with open(tmp_segment_path, "wb") as f:
+                    pickle.dump(item, f)
+                os.rename(tmp_segment_path, segment_path)
 
             bandwidth = os.path.getsize(segment_path) * 8
 
@@ -153,11 +147,10 @@ class StreamingServer:
 if __name__ == "__main__":
     # Initialize server
     ip_addr = "0.0.0.0"
-    zmq_address = "tcp://*:5556"
     port = 8080
     segment_duration = 1.0  # Set segment duration in seconds
 
-    server = StreamingServer(ip_addr, zmq_address=zmq_address, port=port, segment_duration=segment_duration)
+    server = StreamingServer(ip_addr, port=port, segment_duration=segment_duration)
 
     threading.Thread(target=server.start_http_server, daemon=True).start()
 
